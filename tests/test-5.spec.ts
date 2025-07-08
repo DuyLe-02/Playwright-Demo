9488880cc91e5de2bc88d134180946cc6e2933f1
Duy Le - 48334 <duy.le@agest.vn>
2025-07-08 09:14:39 +0700

    Done function of TC5

--- tests/test-5.spec.ts
+++ tests/test-5.spec.ts
@@ -6,6 +6,7 @@ import { CartPage } from "src/pages/CartPage";
 import { CheckoutPage } from "src/pages/CheckoutPage";
 import { ShopPage } from "src/pages/ShopPage";
 import { OrderPage } from "src/pages/OrderPage";
+import { OrderHistoryPage } from "src/pages/OrderHistoryPage";
 import { url, username, password } from "src/utils/env";
 
 test("Verify orders appear in order history", async ({ page }) => {
@@ -16,6 +17,13 @@ test("Verify orders appear in order history", async ({ page }) => {
   const checkoutPage = new CheckoutPage(page);
   const shopPage = new ShopPage(page);
   const orderPage = new OrderPage(page);
+  const orderHistoryPage = new OrderHistoryPage(page);
+
+  //Pre-condition
+  await page.goto(
+    "https://demo.testarchitect.com/checkout/order-received/12256/?key=wc_order_RGIFpc7LynDqN"
+  );
+  const detail = await orderPage.getOrderInfo();
 
   //1. Go to My Account page
   await welcomePage.navigate(url);
@@ -26,4 +34,6 @@ test("Verify orders appear in order history", async ({ page }) => {
   await accountPage.clickOrderTab();
 
   //3. Verify order details
+  const orders = await orderHistoryPage.getOrdersHistoryInfo();
+  expect(detail).toEqual(orders);
 });
