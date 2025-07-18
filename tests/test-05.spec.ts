a36425288dea91d8e5d0e9eb16a5982c9150b420
DuyLe-02 <duy.le@agest.vn>
2025-07-18 15:43:53 +0700

    improve TC5

--- tests/test-05.spec.ts
+++ tests/test-05.spec.ts
@@ -1,22 +1,50 @@
 import { test, expect } from "src/fixtures/fixtures";
 import { url, username, password } from "src/utils/env";
 
+test.beforeEach("Login", async ({ welcomePage, loginPage }) => {
+  await welcomePage.navigate(url);
+  await welcomePage.handlePopupIfVisible();
+  await welcomePage.clickLoginTab();
+  await loginPage.login(username, password);
+});
+
+test.beforeEach(
+  "Create order",
+  async ({
+    myAccountPage,
+    electronicComponentPage,
+    cartPage,
+    checkoutPage,
+  }) => {
+    await myAccountPage.selectElectronicComponent();
+    await electronicComponentPage.addToCart("DJI Mavic Pro Camera Drone");
+    await electronicComponentPage.goToCart();
+    await cartPage.clickCheckoutButton();
+    const checkoutInfo: CheckoutInfo = {
+      firstName: "John",
+      lastName: "Back",
+      countryRegion: "United States (US)",
+      streetAddress: "123 GVN",
+      city: "San Jose",
+      state: "CA",
+      zipCode: "90001",
+      phone: "1234567890",
+      email: "john.back@gmail.com",
+    };
+    await checkoutPage.fillInfo(checkoutInfo);
+    await checkoutPage.clickOrderButton();
+  }
+);
+
 test("Verify orders appear in order history", async ({
-  page,
-  welcomePage,
-  loginPage,
   myAccountPage,
   orderPage,
   orderHistoryPage,
 }) => {
-  //Pre-condition
-
   const detail = await orderPage.getOrderInfo();
 
   //1. Go to My Account page
-  await welcomePage.navigate(url);
-  await welcomePage.clickLoginTab();
-  await loginPage.login(username, password);
+  await myAccountPage.goToMyAccountPage();
 
   //2. Click on Orders in left navigation
   await myAccountPage.clickOrderTab();
