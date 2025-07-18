e33b0516a849221a22e6433ea1cd5e0974e7f61f
DuyLe-02 <duy.le@agest.vn>
2025-07-18 13:47:26 +0700

    create method to handle Popup

--- src/pages/WelcomePage.ts
+++ src/pages/WelcomePage.ts
@@ -4,11 +4,22 @@ export class WelcomePage {
   readonly login_signupTab: Locator = this.page.getByRole("link", {
     name: "Log in / Sign up",
   });
+  readonly adsPopup: Locator = this.page.locator("#popmake-5700");
+  readonly closeAdsButton: Locator = this.page.getByRole("button", {
+    name: "Close",
+  });
 
   constructor(private page: Page) {}
 
   async navigate(url: string): Promise<void> {
     await this.page.goto(url);
+  }
+
+  async handlePopupIfVisible(timeout = 5000): Promise<void> {
+    try {
+      await this.adsPopup.waitFor({ state: "visible", timeout });
+      await this.closeAdsButton.click();
+    } catch (e) {}
   }
 
   async clickLoginTab(): Promise<void> {
